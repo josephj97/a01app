6c730b2b3fba08c405fa94661a54c52f784a965e
joseph_j97 <josephatef.jozza@gmail.com>
2018-03-03 15:33:19 +0200

    First commit

---
+++ src/main/java/com/assignment01/sw2/a01app/A01appApplication.java
@@ -0,0 +1,12 @@
+package com.assignment01.sw2.a01app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A01appApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A01appApplication.class, args);
+	}
+}
---
+++ src/test/java/com/assignment01/sw2/a01app/A01appApplicationTests.java
@@ -0,0 +1,16 @@
+package com.assignment01.sw2.a01app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class A01appApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
